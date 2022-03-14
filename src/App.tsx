9af6d6487997f894e5638377e4608241e00d3bc9
choidy180 <choidy180@naver.com>
2022-03-15 00:44:46 +0900

    first trello update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,51 @@
+import React from "react";
+import { DefaultValue, useRecoilState, useRecoilValue } from "recoil";
+import {createGlobalStyle, DefaultTheme} from "styled-components";
+import reset from "styled-reset";
+import { hourSelector, minuteState } from "./atoms.tsx";
+
+const GlobalStyle = createGlobalStyle<{theme: DefaultTheme}>`
+  ${reset};
+  @font-face {
+      font-family: 'NEXON Lv2 Gothic';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/NEXON Lv2 Gothic.woff') format('woff');
+      font-weight: normal;
+      font-style: normal;
+  }
+  *{
+    font-family: 'NEXON Lv2 Gothic';
+  }
+  body{
+    background-color: #141414;
+    color: white;
+  }
+`
+
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
+    // minutes 세팅
+    setMinutes(+event.currentTarget.value);
+  }
   return (
-    <div className="App">
-    </div>
+    <>
+      <GlobalStyle/>
+      <div>
+        <input 
+          value={minutes} 
+          onChange={onMinutesChange} 
+          type="number" 
+          placeholder="Minutes" 
+        />
+        <input 
+          value={hours} 
+          type="number" 
+          placeholder="Hours" 
+        />
+      </div>
+    </>
   );
 }
 
-export default App;
+export default App
