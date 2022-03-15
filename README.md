c3a82f54bc588e4f8b0cf53c2f248ac2023f888b
choidy180 <choidy180@naver.com>
2022-03-15 23:52:11 +0900

    recoil set

--- README.md
+++ README.md
@@ -5,5 +5,7 @@
 ### 숫자로 받아야 하는경우 input 기본세팅이 string, 앞에 +를 붙혀서 string입력을 number로 바꿀 수 있다. ex) +"1" -> 1<hr>
 ### event.currentTarget.value >> +event.currentTarget.value
 
-### selector(options)
+### 🎉selector(options)
 ### 이 속성이 설정되면 selector는 쓰기 가능한 상태를 반환한다. 첫번째 매개변수로 콜백 객체와 새로 입력 값이 전달된다. 사용자가 selector를 재설정할 경우 새로 입력 값은 T 타입의 값 또는 DefaultValue 타입의 객체일 수 있다.
+
+### set? - 이 속성이 설정되면 selector는 쓰기 가능한 상태를 반환한다. 첫번째 매개변수로 콜백 객체와 새로 입력 값이 전달된다. 사용자가 selector를 재설정할 경우 새로 입력 값은 T 타입의 값 또는 DefaultValue 타입의 객체일 수 있다. 콜백에는 다음이 포함된다.
--- src/App.tsx
+++ src/App.tsx
@@ -23,11 +23,14 @@ const GlobalStyle = createGlobalStyle<{theme: DefaultTheme}>`
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
     // minutes 세팅
     setMinutes(+event.currentTarget.value);
-  }
+  };
+  const onHoursChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setHours(event.currentTarget.value)
+  };
   return (
     <>
       <GlobalStyle/>
@@ -40,6 +43,7 @@ function App() {
         />
         <input 
           value={hours} 
+          onChange={onHoursChange}
           type="number" 
           placeholder="Hours" 
         />
